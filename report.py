77013f87fe91334ee1d24b2cbb8b354fdfe32aac
Stewart Martin-Haugh <stewart.martin-haugh@cern.ch>
2019-08-22 12:44:22 +0100

    Check datetime, refine production category, write to csv

--- report.py
+++ report.py
@@ -1,27 +1,17 @@
-import urllib.request
+import urllib.parse, urllib.request
 import ssl
 import csv
 import datetime
 import re
+import argparse
 
-#now = datetime.datetime.now().date()
-#start_date_str = start_date.strftime('%d.%m.%Y')
-#end_date_str = end_date.strftime('%d.%m.%Y')
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def retrieve_csv(resourcesreporting, start_date, end_date, filename = "output"):
-    url = 'https://bigpanda.cern.ch/api/grafana?table=completed&groupby=dst_experiment_site,error_category&field=sum_count&dst_cloud=UK&resourcesreporting' + resourcesreporting + '&date_from=' + start_date + '%2000:00:00&date_to=' + end_date + '%2000:00:00&export=csv'
+    url = 'https://bigpanda.cern.ch/api/grafana?table=completed&groupby=dst_experiment_site,error_category&field=sum_count&dst_cloud=UK&jobstatus=&jobstatus=finished%7Cfailed%7Ccancelled&resourcesreporting='
+    url += resourcesreporting + '&date_from=' + start_date + '%2000:00:00&date_to=' + end_date + '%2000:00:00&export=csv'
     print("Reading from " + url)
     urllib.request.urlretrieve(url,filename+ '.csv')
-
-
-#url = 'https://bigpanda.cern.ch/api/grafana?table=completed&groupby=dst_experiment_site,error_category&field=sum_count&dst_cloud=UK&resourcesreporting=' + jobtype + '&date_from=' + start_date + '%2000:00:00&date_to=' + end_date + '%2000:00:00&export=csv'
-#print(url)
-##
-#url = 'https://bigpanda.cern.ch/api/grafana?table=completed&groupby=dst_experiment_site,error_category&field=sum_count&dst_cloud=UK&resourcesreporting!=Analysis&date_from=' + start_date + '%2000:00:00&date_to=' + end_date + '%2000:00:00&export=csv'
-#print(url)
-#urllib.request.urlretrieve(url,'production_and_errors.csv')
-
 
 federations = ['UKI-LT2', 'UKI-NORTHGRID', 'UKI-SCOTGRID', 'UKI-SOUTHGRID']
 sites = ['RAL-LCG2',
@@ -55,28 +45,31 @@ def site_name_special_case(site):
 
 
 
-def analyse_csv(filename):
-    pilot_regex = re.compile('Pilot|pilot')
-    get_put_regex = re.compile('DDM')
+def analyse_csv(filename, start_date, end_date):
+    pilot_regex = re.compile('^Pilot')
+    #get_put_regex = re.compile('.*Get error|.*get error|.*Put error|.*put error')
+    get_put_regex = re.compile('Get error|get error|Put error|put error')
     
     
     total_completed = {}
     pilot_errors = {}
     get_put_errors = {}
-    with open(filename + '.csv', 'r') as fp:
-        reader = csv.reader(fp, delimiter=';', quotechar='"')
+    with open(filename + '.csv', 'r') as filein:
+        reader = csv.reader(filein, delimiter=';', quotechar='"')
         rows = [row for row in reader if row[0] != 'dst_experiment_site']
-        #sites =set([row[0] for row in rows])
         errors =set([row[1] for row in rows if row[1] != 'No Error'])
         
     
-        pilot_error_types = [error for error in errors if pilot_regex.match(error)]
-        get_put_error_types = [error for error in errors if get_put_regex.match(error)]
+        pilot_error_types = [error for error in errors if pilot_regex.search(error)]
+        get_put_error_types = [error for error in errors if get_put_regex.search(error)]
         
         #sites first
         for row in rows:
             site = site_name_special_case(row[0])
+            assert site in sites, "%r not in UK cloud site list" % site
             total_completed[site] = total_completed.get(site, 0) + int(row[2])
+            if row[1] == "":
+                print("Unclassified error state: " + site + " " + row[2])
             if row[1] in pilot_error_types:
                 pilot_errors[site] = pilot_errors.get(site, 0) + int(row[2])
             if row[1] in get_put_error_types:
@@ -91,21 +84,45 @@ def analyse_csv(filename):
 
             
                 
-    print(filename)        
-    print("site, total, pilot errors, get/put errors")
-    print("UK-T1-RAL" + ", " + str(total_completed.get('RAL-LCG2',0)) + ", " + str(pilot_errors.get('RAL-LCG2',0)) + ", " + str(get_put_errors.get('RAL-LCG2',0)))
-    for fed in sorted(federations):
-        for site in sites:
-            if fed in site: 
-                print(site + ", " + str(total_completed.get(site,0)) + ", " + str(pilot_errors.get(site,0)) + ", " + str(get_put_errors.get(site,0)))
-        print(fed + ", " + str(total_completed.get(fed,0)) + ", " + str(pilot_errors.get(fed,0)) + ", " + str(get_put_errors.get(fed,0)))
+    with open(filename + "_summed.csv", "w") as output:
+        output.write(filename + " " + start_date + " - " + end_date + "\n")        
+        output.write("site, total, pilot errors, get/put errors\n")
+        output.write("UK-T1-RAL" + ", " + str(total_completed.get('RAL-LCG2',0)) + ", " + str(pilot_errors.get('RAL-LCG2',0)) + ", " + str(get_put_errors.get('RAL-LCG2',0)))
+        output.write("\n")
+        for fed in sorted(federations):
+            for site in sites:
+                if fed in site: 
+                    output.write(site + ", " + str(total_completed.get(site,0)) + ", " + str(pilot_errors.get(site,0)) + ", " + str(get_put_errors.get(site,0)))
+                    output.write("\n")
+            output.write(fed + ", " + str(total_completed.get(fed,0)) + ", " + str(pilot_errors.get(fed,0)) + ", " + str(get_put_errors.get(fed,0)))
+            output.write("\n")
+ 
+    print(open(filename +"_summed.csv").read())
     
 
-start_date = "01.04.2019"
-end_date = "01.07.2019"
-retrieve_csv("=Analysis", start_date, end_date, "analysis")
-retrieve_csv("!=Analysis", start_date, end_date, "production")
-#retrieve_csv("=Data%20Calib%5C%2FMonit%7CData%20Processing%7CEvent%20Index%7CGroup%20Production%7CMC%20Reconstruction%7CMC%20Simulation%7COthers", start_date, end_date, "production")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start',
+    type = str,
+    help ='start date',
+    default = "03.05.2019"
+    )
+    parser.add_argument('--end',
+    type = str,
+    help ='start date',
+    default = "01.07.2019"
+    )
+    args = parser.parse_args()
+    start_date = datetime.datetime.strptime(args.start, '%d.%m.%Y')
+    end_date = datetime.datetime.strptime(args.end, '%d.%m.%Y')
+    assert end_date > start_date, "End date must be after start date"
+    print("Reading data from " + args.start + " to " + args.end)
 
-analyse_csv("analysis")
-analyse_csv("production")
+    retrieve_csv("Analysis", args.start, args.end, "analysis")
+    retrieve_csv("Data%20Calib%5C%2FMonit%7CData%20Processing%7CEvent%20Index%7CGroup%20Production%7CMC%20Reconstruction%7CMC%20Simulation%7COthers", args.start, args.end, "production")
+    
+    analyse_csv("analysis", args.start, args.end)
+    analyse_csv("production", args.start, args.end)
+
+if __name__ == "__main__":
+    main()
