83b3c6b7379f26cd1ee7e70e3e25eed4d9df5690
Stewart Martin-Haugh <stewart.martin-haugh@cern.ch>
2020-01-23 16:51:26 +0000

    Move more calculation to the script from the spreadsheet, reduce copy-paste

--- report.py
+++ report.py
@@ -40,8 +40,19 @@ def site_name_special_case(site):
         return 'UKI-SCOTGRID-ECDF'
     elif site == 'RAL-LCG2-ECHO':
         return 'RAL-LCG2'
-    else: 
+    else:
         return site
+
+
+def write_csv_line(site, tc, pe, gpe, output):
+    output.write(site + ", " + str(tc.get(site,0)) + ", " + str(pe.get(site,0)) + ", " + str(gpe.get(site,0)))
+    job_success = 0.0
+    data_availability = 0.0
+    if (tc.get(site,0) > 0):
+        job_success = 1.0 - (pe.get(site,0)/tc.get(site,0))
+        data_availability = 1.0 - (gpe.get(site,0)/tc.get(site,0))
+    output.write(", " + str(job_success) + "," + str(data_availability))
+    output.write("\n")
 
 
 
@@ -49,8 +60,8 @@ def analyse_csv(filename, start_date, end_date):
     pilot_regex = re.compile('^Pilot')
     #get_put_regex = re.compile('.*Get error|.*get error|.*Put error|.*put error')
     get_put_regex = re.compile('Get error|get error|Put error|put error')
-    
-    
+
+
     total_completed = {}
     pilot_errors = {}
     get_put_errors = {}
@@ -58,11 +69,11 @@ def analyse_csv(filename, start_date, end_date):
         reader = csv.reader(filein, delimiter=';', quotechar='"')
         rows = [row for row in reader if row[0] != 'dst_experiment_site']
         errors =set([row[1] for row in rows if row[1] != 'No Error'])
-        
-    
+
+
         pilot_error_types = [error for error in errors if pilot_regex.search(error)]
         get_put_error_types = [error for error in errors if get_put_regex.search(error)]
-        
+
         #sites first
         for row in rows:
             site = site_name_special_case(row[0])
@@ -81,36 +92,42 @@ def analyse_csv(filename, start_date, end_date):
                         pilot_errors[fed] = pilot_errors.get(fed, 0) + int(row[2])
                     if row[1] in get_put_error_types:
                         get_put_errors[fed] = get_put_errors.get(fed, 0) + int(row[2])
-
             
-                
+        #T2 totals
+        for site in sites:
+            if site!="RAL-LCG2":
+                total_completed["Total T2"] = total_completed.get("Total T2", 0) + int(row[2])
+                pilot_errors["Total T2"] = pilot_errors.get("Total T2", 0) + int(row[2])
+                get_put_errors["Total T2"] = get_put_errors.get("Total T2", 0) + int(row[2])
+
+
+
     with open(filename + "_summed.csv", "w") as output:
-        output.write(filename + " " + start_date + " - " + end_date + "\n")        
-        output.write("site, total, pilot errors, get/put errors\n")
-        output.write("UK-T1-RAL" + ", " + str(total_completed.get('RAL-LCG2',0)) + ", " + str(pilot_errors.get('RAL-LCG2',0)) + ", " + str(get_put_errors.get('RAL-LCG2',0)))
-        output.write("\n")
+        output.write(filename + " " + start_date + " - " + end_date + "\n")
+        output.write(", , Completed Jobs, Failed Jobs (Pilot), Failed Jobs (Get/Put), Job Success rate, Data Availability\n")
+        write_csv_line("RAL-LCG2", total_completed, pilot_errors, get_put_errors, output)
         for fed in sorted(federations):
             for site in sites:
-                if fed in site: 
-                    output.write(site + ", " + str(total_completed.get(site,0)) + ", " + str(pilot_errors.get(site,0)) + ", " + str(get_put_errors.get(site,0)))
-                    output.write("\n")
-            output.write(fed + ", " + str(total_completed.get(fed,0)) + ", " + str(pilot_errors.get(fed,0)) + ", " + str(get_put_errors.get(fed,0)))
-            output.write("\n")
- 
+                if fed in site:
+                    write_csv_line(site, total_completed, pilot_errors, get_put_errors, output)
+
+            write_csv_line(fed, total_completed, pilot_errors, get_put_errors, output)
+        write_csv_line("Total T2", total_completed, pilot_errors, get_put_errors, output)
+
     print(open(filename +"_summed.csv").read())
-    
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--start',
     type = str,
     help ='start date',
-    default = "03.05.2019"
+    default = "01.10.2019"
     )
     parser.add_argument('--end',
     type = str,
-    help ='start date',
-    default = "01.07.2019"
+    help ='end date',
+    default = "01.01.2020"
     )
     args = parser.parse_args()
     start_date = datetime.datetime.strptime(args.start, '%d.%m.%Y')
@@ -120,7 +137,7 @@ def main():
 
     retrieve_csv("Analysis", args.start, args.end, "analysis")
     retrieve_csv("Data%20Calib%5C%2FMonit%7CData%20Processing%7CEvent%20Index%7CGroup%20Production%7CMC%20Reconstruction%7CMC%20Simulation%7COthers", args.start, args.end, "production")
-    
+
     analyse_csv("analysis", args.start, args.end)
     analyse_csv("production", args.start, args.end)
 
